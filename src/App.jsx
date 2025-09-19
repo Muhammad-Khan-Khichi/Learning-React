00e2a5ca59b41b4e9eda4c9dd9001bb48126bc08
Muhammad Khan <muhammadkhan08561@gmail.com>
2025-09-19 16:40:35 +0500

    wrapup the project

--- src/App.jsx
+++ src/App.jsx
@@ -28,10 +28,10 @@ function App() {
     return <AnimalShow type={animal} key={idx}/>
   })
   return (
-    <>
+    <div className="flex flex-col items-center">
     <button className="bg-green-400 p-1 cursor-pointer" onClick={handleClick}>Add Animal</button>
-    <div>{renderAnimals}</div>
-    </>
+    <div className="flex flex-row flex-wrap justify-center">{renderAnimals}</div>
+    </div>
   );
 }
 
--- src/components/AnimalShow.jsx
+++ src/components/AnimalShow.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import cat from '../assets/cat.svg'
 import cow from '../assets/cow.svg'
 import dog from '../assets/dog.svg'
@@ -18,9 +18,14 @@ const svgMap = {
 }
 
 function AnimalShow({type}) {
+    const [clicks, setClicks] = useState(0)
+    const handleClick = () => {
+        setClicks(clicks + 1)
+    }
   return (
-    <div>
+    <div onClick={handleClick}>
         <img src="{svgMap[type]}" alt="animal" />
+        <img className='w-2.5' src="{heart}" alt="heart" />
     </div>
   )
 }
